7bdcea80354f72af27d26f23afc1ddf22b5a0aa1
VeranikaBabik <veranikababik@gmail.com>
2023-06-10 00:47:04 +0200

    added genres list

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,5 @@
 const API_KEY = `d793dd4ca6e7be6c8e5a071661ccb72e`;
-const API_URL = `https://api.themoviedb.org/3/`;
+const API_URL = `https://api.themoviedb.org/3`;
 import Notiflix from 'notiflix';
 
 export async function getBySearch({ query = '', page = '1' }) {
@@ -32,4 +32,11 @@ export async function getInfoAboutMovie(movieId, query) {
   }
   const { results: movies } = await response.json();
   return movies;
+}
+export async function listOfGenres() {
+  const response = await fetch(
+    `${API_URL}/genre/movie/list?api_key=${API_KEY}`
+  );
+  const { genres: genre } = await response.json();
+  return genre;
 }
