9dfebfbbc61f5b55202a127316322003862700a1
WeronikaBabik <weronika.babik@outlook.com>
2023-06-14 12:45:17 +0000

    Deploying to gh-pages from @ WeronikaBabik/project-js-group-5@83e9056a417616ab1a835a98f5846610d586bfd7 🚀

---
+++ library.6029ec8e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.getElementById("watchedButton"),e=document.getElementById("queueButton");t.addEventListener("click",(function(){t.classList.add("active"),e.classList.remove("active")})),e.addEventListener("click",(function(){e.classList.add("active"),t.classList.remove("active")}))}();
+//# sourceMappingURL=library.6029ec8e.js.map
---
+++ library.bc618bd1.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("watchedButton"),e=document.getElementById("queueButton");t.addEventListener("click",(function(){t.classList.add("active"),e.classList.remove("active")})),e.addEventListener("click",(function(){e.classList.add("active"),t.classList.remove("active")}));
+//# sourceMappingURL=library.bc618bd1.js.map
