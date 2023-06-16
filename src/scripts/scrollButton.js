4fd139144009ce801e52794c380b9bcc18e8f14a
Łukasz Michalak <l.michalak.firma@gmail.com>
2023-06-16 17:45:17 +0200

    scroll button
    adding scrollbutton, arrow.svg, some required html

---
+++ src/scripts/scrollButton.js
@@ -0,0 +1,9 @@
+
+const scrollButton = document.querySelector('.scroll');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    scrollButton.classList.add('scroll--active');
+  } else {
+    scrollButton.classList.remove('scroll--active');
+  }
+});
