f107f203b3b2f8828c5a06d93dfc85076452c043
Łukasz Michalak <l.michalak.firma@gmail.com>
2023-06-21 13:26:59 +0200

    darkmode
    1. keeping darkmode on both main and library when switched
    2. library movie titles on darkmode set to white color

--- src/scripts/switchBtn.js
+++ src/scripts/switchBtn.js
@@ -1,18 +1,21 @@
-//switch btn
 let theme = localStorage.getItem('theme') || 'light';
+let themeLibrary = localStorage.getItem('theme-library') || 'light-library';
 const btnSwitch = document.querySelector('.btn-switch');
-btnSwitch.addEventListener('click', function(){
-    if (theme === 'dark') {
-        document.querySelector('body').classList.remove('dark');
-        theme = 'light';
-    } else {
-        document.querySelector('body').classList.add('dark');
-        theme = 'dark';
-    }
-    localStorage.setItem('theme', theme);
 
+btnSwitch.addEventListener('click', function () {
+  if (theme === 'dark') {
+    document.querySelector('body').classList.remove('dark');
+    theme = 'light';
+    themeLibrary = 'light-library';
+  } else {
+    document.querySelector('body').classList.add('dark');
+    theme = 'dark';
+    themeLibrary = 'dark-library';
+  }
+  localStorage.setItem('theme', theme);
+  localStorage.setItem('theme-library', themeLibrary);
 });
 
 if (theme === 'dark') {
-    document.querySelector('body').classList.add('dark');
-};
+  document.querySelector('body').classList.add('dark');
+}
--- src/scripts/switchBtnLibrary.js
+++ src/scripts/switchBtnLibrary.js
@@ -1,16 +1,21 @@
 let themeLibrary = localStorage.getItem('theme-library') || 'light-library';
+let theme = localStorage.getItem('theme') || 'light';
 const btnSwitchLibrary = document.querySelector('.btn-switch-library');
-btnSwitchLibrary.addEventListener('click', function(){
-    if (themeLibrary === 'dark-library') {
-        document.querySelector('body').classList.remove('dark-library');
-        themeLibrary = 'light-library';
-    } else {
-        document.querySelector('body').classList.add('dark-library');
-        themeLibrary = 'dark-library';
-    }
-    localStorage.setItem('theme-library', themeLibrary);
+
+btnSwitchLibrary.addEventListener('click', function () {
+  if (themeLibrary === 'dark-library') {
+    document.querySelector('body').classList.remove('dark-library');
+    themeLibrary = 'light-library';
+    theme = 'light';
+  } else {
+    document.querySelector('body').classList.add('dark-library');
+    themeLibrary = 'dark-library';
+    theme = 'dark';
+  }
+  localStorage.setItem('theme-library', themeLibrary);
+  localStorage.setItem('theme', theme);
 });
 
 if (themeLibrary === 'dark-library') {
-    document.querySelector('body').classList.add('dark-library');
-};
+  document.querySelector('body').classList.add('dark-library');
+}
